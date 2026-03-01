a1aab93038d2a6baf7796c1f46f1a44e331ff5e0
Maryem <maryemgassri3@gmail.com>
2026-03-01 14:14:50 +0100

    modified database

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:mimi2004@localhost:5432/pfe_db"
+DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/pfe_db"
 
 engine = create_engine(DATABASE_URL)
 
